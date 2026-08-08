04de3507e1a600082d1f2dea346f425718cda9e0
computerid-dev <nugrohokelyn@gmail.com>
2026-08-08 19:06:58 +0700

    Upload app/src/main/java/com/echochat/cid/data/AppDatabase.kt

---
+++ app/src/main/java/com/echochat/cid/data/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.echochat.cid.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Friend::class, Message::class, GroupChat::class, GroupMessage::class],
+    version = 3,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun friendDao(): FriendDao
+    abstract fun messageDao(): MessageDao
+    abstract fun groupDao(): GroupDao
+    abstract fun groupMessageDao(): GroupMessageDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "echochat.db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build().also { instance = it }
+            }
+        }
+    }
+}
